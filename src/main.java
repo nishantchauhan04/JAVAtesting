efdb2bdb0052bfae6ce6b5639b6d0b58ce3f0138
harshbajaj16 <35989538+harshbajaj16@users.noreply.github.com>
2018-03-15 12:32:46 +0530

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,16 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=10;
+		int c=a+b;
+		int d=a*b;
+		int e=a-b;
+		int f=a/b;
+System.out.println("A="+a+" and B="+b+"\n"+"Sum is " +c +"\n"+ "subtract is "
+		+e +"\n"+"multiply is "+d+"\n"+"Division is "+f );
+	}
+
+}
